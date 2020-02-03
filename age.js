42d4895dd8b33fe3bfadbb4f4b150dcee0ac13c2
heavenllord <60015740+heavenllord@users.noreply.github.com>
2020-02-03 14:50:37 -0600

    Update age.js

--- age.js
+++ age.js
@@ -1,12 +1,13 @@
    var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
      floatAge = parseFloat(prompt("Enter Age"));
-     floatDays = parseFloat(floatAge*float365.25);
-     floatMonths = parseFloat(floatAge*float12);
-     intWeeks = parseInt(floatsDays/float7);
-     intFortnights = parseInt(floatDays/float14);
+     floatDays = parseFloat(floatAge*365.25);
+     floatMonths = parseFloat(floatAge*12);
+     intWeeks = parseInt(floatsDays/7);
+     intFortnights = parseInt(floatDays/14);
      alert("Your age is: " + floatAge);
      alert("Your age is: " + floatDays);
      alert("Your age is: " + floatMonths);
      alert("Your age is: " + intWeeks);
      alert("Your age is: " + intFortnights);
-/*This program will calculate foatDays by multipying age x 365.25*/
+/*This program will...*/
+   //Calculate floatDays by multiply age x 365.25)
