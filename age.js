676566db909c4f20d3efbe8b34f2ae9bc461a8ca
heavenllord <60015740+heavenllord@users.noreply.github.com>
2020-02-03 14:17:43 -0600

    Add files via upload

--- age.js
+++ age.js
@@ -1,5 +1,11 @@
-   var floatMiles, floatGallons, floatMpg;
-     floatMiles = parseFloat(prompt("Enter miles driven"));
-     floatGallons = parseFloat(prompt("Enter gallons of gas used"));
-     floatMpg = floatMiles/floatGallons;
-     alert("Miles per gallon = " + floatMpg);
+   var floatAge, floatDays, intWeeks, floattMonths, intFortnights;
+     floatAge = parseFloat(prompt("Enter Age"));
+     floatDays = parseFloat(floatAge*365.25);
+     floatMonths = parseFloat(floatAge*12);
+     intWeeks = parseInt(floatsDays/7);
+     intFortnights = parseInt(floatDays/14);
+     alert("Your age is: " + floatAge);
+     alert("Your age is: " + floatDays);
+     alert("Your age is: " + floatMonths);
+     alert("Your age is: " + intWeeks);
+     alert("Your age is: " + intFortnights);
