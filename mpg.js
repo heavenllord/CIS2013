b971eafa6b7058cc6c779a30cdce3afa22b084fc
heavenllord <60015740+heavenllord@users.noreply.github.com>
2020-02-03 13:35:56 -0600

    Add files via upload

--- mpg.js
+++ mpg.js
@@ -1,5 +1,5 @@
-   var floatMiles, floatGallons, floatmpg;
-     .floatMiles = Parsefloat(prompt("Enter miles driven"));
-     .floatGallons = prompt("Enter gallons of gas used");
-     .Floatmpg = floatmiles/floatgallons;
-     alert("Miles per gallon = " + floatmpg);
+   var floatMiles, floatGallons, floatMpg;
+     floatMiles = parseFloat(prompt("Enter miles driven"));
+     floatGallons = parseFloat(prompt("Enter gallons of gas used"));
+     floatMpg = floatMiles/floatGallons;
+     alert("Miles per gallon = " + floatMpg);
